3152a3e2568d9271e71f83c92227701b34334d2b
Brian Wells <briswells@gmail.com>
2020-12-04 16:35:09 -0800

    Implemented TikTakToe and unit tests

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -1,4 +1,5 @@
 #include "TicTacToeBoard.h"
+#include <iostream>
 /**
  * Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
  * to represent the spaces on the board.
@@ -19,20 +20,34 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  Piece cur = turn;
+  if(turn == X){
+    turn = O;
+  }
+  else{
+    turn = X;
+  }
+  return cur;
 }
 
 /**
  * Places the piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. placePiece does 
+ * piece is placed, and toggles which Piece's turn it is. placePiece does
  * NOT allow to place a piece in a location where there is already a piece.
- * In that case, placePiece just returns what is already at that location. 
+ * In that case, placePiece just returns what is already at that location.
  * Out of bounds coordinates return the Piece Invalid value. When the game
  * is over, no more pieces can be placed so attempting to place a piece
  * should neither change the board nor change whose turn it is.
-**/ 
+**/
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
+  if((row < 3 && row >= 0) && (column < 3 && column >= 0)){
+    if(board[row][column] == Blank){
+      board[row][column] = turn;
+      toggleTurn();
+      return board[row][column];
+    }
+  }
   return Invalid;
 }
 
@@ -42,6 +57,9 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
+  if((row < 3 && row >= 0) && (column < 3 && column >= 0)){
+    return board[row][column];
+  }
   return Invalid;
 }
 
@@ -51,5 +69,19 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
+  for(int i = 0; i < BOARDSIZE; ++i){
+    if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != Blank){
+      return board[i][0];
+    }
+    else if(board[0][i] == board[1][i] && board[2][i] == board[2][i] && board[i][0] != Blank){
+      return board[i][0];
+    }
+  }
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != Blank){
+    return board[0][0];
+  }
+  if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != Blank){
+    return board[1][1];
+  }
   return Invalid;
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -4,19 +4,121 @@
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+#include <iostream>
 class TicTacToeBoardTest : public ::testing::Test
 {
 	protected:
 		TicTacToeBoardTest(){} //constructor runs before each test
 		virtual ~TicTacToeBoardTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 /* EXAMPLE TEST FORMAT
 TEST(TicTacToeBoardTest, unitTestName)
 {
-	ASSERT_TRUE(true);
+TicTacToeBoardTest();
+SetUp();
+
+TearDown();
+~TicTacToeBoardTest();
+ASSERT_TRUE(true);
 }
 */
+TEST(TicTacToeBoardTest, blankBoard)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.getWinner();
+  TearDown();
+	ASSERT_TRUE(temp == Invalid);
+}
+
+TEST(TicTacToeBoardTest, toggleTurn)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.toggleTurn();
+  TearDown();
+	ASSERT_TRUE(temp == X);
+}
+
+TEST(TicTacToeBoardTest, placePieceCorrect)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(0,0);
+  TearDown();
+	ASSERT_TRUE(temp == X);
+}
+
+TEST(TicTacToeBoardTest, placePieceInvalidLocation)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(-1,0);
+  TearDown();
+	ASSERT_TRUE(temp == Invalid);
+}
+
+TEST(TicTacToeBoardTest, placePieceAlreadyPlaced)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(0,0);
+	temp = board.placePiece(0,0);
+  TearDown();
+	ASSERT_TRUE(temp == Invalid);
+}
+
+TEST(TicTacToeBoardTest, WinUp)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(0,0);
+	temp = board.placePiece(0,1);
+	temp = board.placePiece(1,0);
+	temp = board.placePiece(0,2);
+	temp = board.placePiece(2,0);
+	temp = board.getWinner();
+  TearDown();
+	ASSERT_TRUE(temp == X);
+}
+
+TEST(TicTacToeBoardTest, WinSidewise)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(0,0);
+	temp = board.placePiece(1,0);
+	temp = board.placePiece(0,1);
+	temp = board.placePiece(1,1);
+	temp = board.placePiece(0,2);
+	temp = board.getWinner();
+  TearDown();
+	ASSERT_TRUE(temp == X);
+}
+
+TEST(TicTacToeBoardTest, WinDiag)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(0,0);
+	temp = board.placePiece(1,0);
+	temp = board.placePiece(1,1);
+	temp = board.placePiece(2,1);
+	temp = board.placePiece(2,2);
+	temp = board.getWinner();
+  TearDown();
+	ASSERT_TRUE(temp == X);
+}
+
+TEST(TicTacToeBoardTest, GetPiece)
+{
+  SetUp();
+  TicTacToeBoard board;
+  Piece temp = board.placePiece(0,0);
+	temp = board.getPiece(0,0);
+  TearDown();
+	ASSERT_TRUE(temp == X);
+}
